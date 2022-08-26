92f6aa8a281716695a2f2e719ac5b151195cab83
yuuahmad <ahmadyusufmaulana0@gmail.com>
2022-08-26 18:20:46 +0700

    final stage

--- src/main.cpp
+++ src/main.cpp
@@ -42,8 +42,7 @@ const int enKiri = 8;
 const int pwmKanan = 6;
 const int pwmKiri = 9;
 
-// TERIMA DADTA
-bool parsing = false;
+// TERIMA DADTAl;;;;;;]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]''''''''''''''\0bool parsing = false;
 String sData, data[10];
 
 void setup()
